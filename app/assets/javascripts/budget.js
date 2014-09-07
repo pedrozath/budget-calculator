ef5520217ca1f9dd19c12edbc72559df34b3e395
Pedro Maciel <pedrozath@gmail.com>
2014-09-07 02:47:31 -0300

    update

---
+++ app/assets/javascripts/budget.js
@@ -0,0 +1,42 @@
+"use strict";
+
+var Budget = function(opt){
+    var o; for(o in opt) this[o]=opt[o];
+    this.bind_events();
+};
+
+Budget.prototype = {
+    bind_events: function(){
+        var _this = this;
+        this.fields.on("keyup", function(){
+            _this.change_callback.call(this, _this.calculate());
+        });
+    },
+
+    calculate: function(){
+        var hour_income = this.get_value("monthly-income")/22/8;
+        var hours = this.get_value("hours");
+        var risk = this.get_value("risk");
+        var profit = this.get_value("profit");
+        var tax = this.get_value("tax");
+        var urgency = this.get_value("urgency");
+        var trading = this.get_value("trading");
+        var taxes = (risk+profit+tax+urgency+trading)/100;
+        var final_value = (hour_income * hours) / (1-taxes);
+
+        if(final_value > 0) { 
+            return this.format_money(final_value)
+        } else {
+            return "Os campos foram preenchidos corretamente?"
+        }
+    },
+
+    format_money: function(value){
+        return "R$ "+String(value).replace(/([^.]*)\.([^.]{2}).*/, "$1,$2");
+    },
+
+    get_value: function(name){
+        var value = parseInt(this.fields.filter("[name='"+name+"']").val());
+        if(isNaN(value)) return 0; else return value;
+    }
+};
